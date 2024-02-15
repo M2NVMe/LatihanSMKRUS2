80e674cc3b96fa88eef2e8e9722a6f9e3d01a0bf
M2NV <m2sjmail@gmail.com>
2024-02-15 09:36:58 +0700

    No Comment itu sih derita elo masa bodo ga mau tau

--- app/src/main/java/com/example/latihan2/ListGame.java
+++ app/src/main/java/com/example/latihan2/ListGame.java
@@ -4,8 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListGame extends AppCompatActivity {
 
+public class ListGame extends AppCompatActivity {
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getJenis() {
+        return Jenis;
+    }
+
+    public void setJenis(String jenis) {
+        Jenis = jenis;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    String Name;
+    String Jenis;
+    int img;
+    public ListGame(String Name, String Jenis, int img) {
+        this.Name = Name;
+        this.Jenis = Jenis;
+        this.img = img;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

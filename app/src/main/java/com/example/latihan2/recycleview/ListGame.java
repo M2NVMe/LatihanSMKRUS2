d91e79249a060b13e2dd049035638da284ecaaae
M2NV <m2sjmail@gmail.com>
2024-02-22 07:55:45 +0700

    No Comment itu sih derita elo masa bodo ga mau tau

--- app/src/main/java/com/example/latihan2/recycleview/ListGame.java
+++ app/src/main/java/com/example/latihan2/recycleview/ListGame.java
@@ -21,8 +21,8 @@ public class ListGame extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycl);
         List<itemsme> Barang = new ArrayList<itemsme>();
         Barang.add(new itemsme("Derail Valley", "Simulation", R.drawable.wauw));
-        Barang.add(new itemsme("Gmod", "Sandbox", R.drawable.wauw));
-        Barang.add(new itemsme("HL2", "Adventure", R.drawable.wauw));
+        Barang.add(new itemsme("Gmod", "Sandbox", R.drawable.gm));
+        Barang.add(new itemsme("HL2", "Adventure", R.drawable.hl2));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new adaptor(getApplicationContext(),Barang));
